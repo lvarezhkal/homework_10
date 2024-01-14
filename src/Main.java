be2db1d22c7e719c969765106777420523405ccc
lvarezhkal <kkakxaa@gmail.com>
2024-01-14 16:00:08 +0300

    allDone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        //1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        //2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //3
+        String completeName = "Иванов Семён Семёнович";
+        completeName = completeName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + completeName);
     }
 }
